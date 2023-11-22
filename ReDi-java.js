75f5d443b571a69d5e4d31e3a77d4d2f87fdcf91
Sefka Neziri <sefka.neziri@hotmail.com>
2023-11-22 20:07:22 +0100

    S

--- ReDi-java.js
+++ ReDi-java.js
@@ -2,3 +2,24 @@ function showMeni() {
     document.getElementById("meni").classList.toggle("show");
 }
 // Zatvori meni klikom izvan njega
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
