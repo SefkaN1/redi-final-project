2a3de024b6df0f651caf0adc60083334056855a2
Sefka Neziri <sefka.neziri@hotmail.com>
2023-11-27 18:18:53 +0100

    page2

---
+++ Java-scr.js
@@ -0,0 +1,16 @@
+// Logout button
+document.getElementById('logout-button').addEventListener('click', function() {
+    alert('Logging out...');
+}); 
+// Resume button
+ document.getElementById('resume-button').addEventListener('click', function() {
+    alert('Redirecting to My Resume...');
+});
+//Saved button
+document.getElementById('saved-button').addEventListener('click', function() {
+    alert('Redirecting to saved vacancies...');
+});
+//Responses
+document.getElementById('response-button').addEventListener('click', function() {
+    alert('Redirecting to My responses...');
+});
